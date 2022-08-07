d769668fe99c629a32057120a189455978625fbb
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-08-08 00:41:39 +0530

    Update rss_sql.py

--- AmeliaRobot/modules/sql/rss_sql.py
+++ AmeliaRobot/modules/sql/rss_sql.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, UnicodeText
 
 class RSS(BASE):
     __tablename__ = "rss_feed"
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
     chat_id = Column(UnicodeText, nullable=False)
     feed_link = Column(UnicodeText)
     old_entry_link = Column(UnicodeText)
